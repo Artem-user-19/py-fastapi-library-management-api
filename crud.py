89cdc19074910dda1e41de3e2a478a18d0e7170e
Artem <temakot254@gmail.com>
2024-07-07 08:49:16 +0300

    Solution

--- crud.py
+++ crud.py
@@ -1,17 +1,23 @@
+# crud.py
 from sqlalchemy.orm import Session
 import models
 import schemas
+from typing import List, Optional
 
 
-def get_author(db: Session, author_id: int):
+def get_author(db: Session, author_id: int) -> Optional[models.Author]:
     return db.query(models.Author).filter(models.Author.id == author_id).first()
 
 
-def get_authors(db: Session, skip: int = 0, limit: int = 10):
+def get_author_by_name(db: Session, name: str) -> Optional[models.Author]:
+    return db.query(models.Author).filter(models.Author.name == name).first()
+
+
+def get_authors(db: Session, skip: int = 0, limit: int = 10) -> List[type[models.Author]]:
     return db.query(models.Author).offset(skip).limit(limit).all()
 
 
-def create_author(db: Session, author: schemas.AuthorCreate):
+def create_author(db: Session, author: schemas.AuthorCreate) -> models.Author:
     db_author = models.Author(name=author.name, bio=author.bio)
     db.add(db_author)
     db.commit()
@@ -19,15 +25,15 @@ def create_author(db: Session, author: schemas.AuthorCreate):
     return db_author
 
 
-def get_books(db: Session, skip: int = 0, limit: int = 10):
+def get_books(db: Session, skip: int = 0, limit: int = 10) -> List[type[models.Book]]:
     return db.query(models.Book).offset(skip).limit(limit).all()
 
 
-def get_books_by_author(db: Session, author_id: int, skip: int = 0, limit: int = 10):
+def get_books_by_author(db: Session, author_id: int, skip: int = 0, limit: int = 10) -> List[type[models.Book]]:
     return db.query(models.Book).filter(models.Book.author_id == author_id).offset(skip).limit(limit).all()
 
 
-def create_book(db: Session, book: schemas.BookCreate):
+def create_book(db: Session, book: schemas.BookCreate) -> models.Book:
     db_book = models.Book(
         title=book.title,
         summary=book.summary,
